3f872f2296e7487172247d8d7cfcdcf28c66d24d
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-12-14 23:19:45 -0700

    Create redirect.js

---
+++ random/redirect.js
@@ -0,0 +1,5 @@
+// Check if the current URL matches the specified pattern
+if (window.location.href.startsWith('https://jjsal1234.github.io/data/random/')) {
+    // Redirect to the desired URL
+    window.location.href = 'https://jjsal1234.github.io/data/';
+}
